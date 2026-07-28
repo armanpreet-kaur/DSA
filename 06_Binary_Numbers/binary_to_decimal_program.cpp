f1b1182faaf92ee2c52286baa062538a8ea29206
Armanpreet Kaur <kaurarman8019@gmail.com>
2026-07-28 14:54:50 +0530

    Added binary to decimal program

---
+++ 06_Binary_Numbers/binary_to_decimal_program.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int binary;
+    cout <<"enter binary: ";
+    cin >> binary;
+
+    int decimal = 0;
+    int power = 0;
+    
+    while(binary > 0){
+        int digit = binary % 10;
+        decimal = decimal + digit * pow(2,power);
+
+        power++ ;
+        binary = binary/10;
+
+    }
+    cout << decimal;
+    return 0;
+}
