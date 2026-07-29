08896da299a492fbeee6c1e40d2c49badfa22d68
Armanpreet Kaur <kaurarman8019@gmail.com>
2026-07-29 15:33:35 +0530

    Added count a vowels program

---
+++ 14_Strings/05_count_vowels.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout <<"Enter a string:";
+    getline(cin,str);
+
+    int count = 0;
+
+    for(int i =0; i <str.length(); i++){
+        if ( str[i] == 'a' ||  str[i] == 'e' ||  str[i] == 'i'||  str[i] == 'o' || 
+            str[i] == 'u'||  str[i] == 'A' ||  str[i] == 'E' ||  str[i] == 'I' ||  str[i] == 'O' ||  str[i] == 'U')
+            {
+                count++ ;
+
+            }
+            
+    }
+    cout <<"Total vowels: " <<count;
+    return 0;
+}
