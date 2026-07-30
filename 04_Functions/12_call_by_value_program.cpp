d4a8c86f577768dddb7d06922aaf42d13edd6ee1
Armanpreet Kaur <kaurarman8019@gmail.com>
2026-07-30 13:04:36 +0530

    Added call by value program

---
+++ 04_Functions/12_call_by_value_program.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void change(int x){
+    x = 100;
+    cout << "Inside function: " << x <<endl;
+}
+int main()
+{
+    int a=10;
+    change(a);
+    cout <<"Inside Main: "<<a;
+    return 0;
+}
