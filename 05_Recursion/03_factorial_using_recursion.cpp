cae2e0b20212441ada8814a1ecc199fdead653cd
Armanpreet Kaur <kaurarman8019@gmail.com>
2026-07-30 13:51:17 +0530

    Added sum of first n natural number program

---
+++ 05_Recursion/03_factorial_using_recursion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    if (n == 0){
+        return 1;
+    }
+    return n * factorial(n-1);
+}
+int main()
+{
+    int n;
+    cout <<"enter a number: ";
+    cin >>n;
+
+    cout << "Factorial: " <<factorial(9);
+    return 0;
+}
