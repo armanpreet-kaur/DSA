c5bc4be5026da5f32f1b7e791b5be1299856a67f
Armanpreet Kaur <kaurarman8019@gmail.com>
2026-08-03 15:21:42 +0530

    Added vector empty program

---
+++ 16_Vectors/05_vector_empty.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+
+    if(v.empty())
+    {
+        cout << "Vector is empty";
+    }
+
+    return 0;
+}
